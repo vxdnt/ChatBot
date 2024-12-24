7da28b28150723df3429208465813130ff55de79
vxdnt <vedantkonde36@gmail.com>
2024-12-24 22:52:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from threading import Timer
 from pymongo import ReturnDocument
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import random
+import secrets
+import string
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -80,7 +83,9 @@ def restart():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    user_id = data.get("user_id")
+    characters = string.ascii_letters + string.digits
+    # Generate a secure random string of the specified length
+    user_id = ''.join(secrets.choice(characters) for _ in range(6))
     user_message = data.get("message", "").lower()
 
     if not user_id:
